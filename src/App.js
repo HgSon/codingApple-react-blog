8849b4073504d62bcce82195452ab9545b16459e
손한결 <hgson.8@gmail.com>
2022-06-24 11:11:21 +0900

    state 변경

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import logo from './logo.svg';
+/*eslint-disable*/  //워닝메시지 뜨지 않도록.
+
 import './App.css';
 import React, {useState} from "react";
 
 
 function App() {
 
-  const logo = "ReactBlog";
+	const logo = "ReactBlog";
 
-  let [titles, setTitle] = useState(["여자 코트 추천", "역삼 맛집 모음", "파이썬 블로그"]);
-  let [dates, setDate] = useState(["6월 24일", "6월 25일", "7월 8일"])
+	let [titles, setTitle] = useState(["여자 코트 추천", "역삼 맛집 모음", "파이썬 블로그"]);
+	let [dates, setDate] = useState(["6월 24일", "6월 25일", "7월 8일"]);
+    let [likes, setLikes] = useState([0, 0, 0]);
+
+    const handleClick = (index) => {
+    	let newLikes = [...likes];
+    	newLikes[index]++;
+    	setLikes(newLikes)
+
+    }
 
 
-  return (
-    <div className="App">
-      <div className="black-nav">
-          <h4>{ logo }</h4>
-      </div>
-        {titles.map((title, index) => (
-            <div className="list">
-                <h4>{ title }</h4>
-                <p>{dates[index]} 발행</p>
-            </div>
-        ))}
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="black-nav">
+				<h4>{logo}</h4>
+			</div>
+			{titles.map((title, index) => (
+				<div className="list" key={title}>
+					<h4>{title} <span onClick={() => handleClick(index)}>👍</span> {likes[index]} </h4>
+					<p>{dates[index]} 발행</p>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default App;
